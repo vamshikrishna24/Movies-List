808d06961fca4c9ff52d46f7ec02fe00db011121
vamshikrishna24 <vamshikrishna2124@gmail.com>
2023-01-06 14:43:14 +0530

    movieForm updated

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,10 +1,24 @@
 import http from "./httpService";
 import config from "../config.json";
 
+const apiEndPoint = config.apiUrl + "/movies";
 export function getMovies() {
-  return http.get(config.apiUrl + "/movies");
+  return http.get(apiEndPoint);
+}
+
+export function getMovie(movieId) {
+  return http.get(apiEndPoint + "/" + movieId);
+}
+
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(apiEndPoint + "/" + movie._id, body);
+  }
+  return http.post(apiEndPoint, movie);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(config.apiUrl + "movies/" + movieId);
+  return http.delete(apiEndPoint + "/" + movieId);
 }
